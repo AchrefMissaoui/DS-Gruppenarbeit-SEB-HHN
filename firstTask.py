e68981cccb478575942382e8d5b2b23a0a299328
Achref Missaoui <missaoui@stud.hs-heilbronn.de>
2021-06-11 13:00:36 +0200

    first task

---
+++ firstTask.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+dict_from_csv = pd.read_csv('data.csv',sep=";").to_dict()
+
+
+
+dict = {}
+
+for i in dict_from_csv["Customer"]:
+
+    dict[dict_from_csv["Customer"][i]] = {
+        "Country" : dict_from_csv["Country"][i],
+        "Revenue" : dict_from_csv["Revenue"][i],
+        "Currency": dict_from_csv["Currency"][i]
+    }
+
+for customer in dict:
+    print("Umsatz",customer,":", dict[customer]["Revenue"])
+
+
